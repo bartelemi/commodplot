aaf4ca725cd8ecf0b3d08172ba17e1f6a58389dc
UI906879 <Pear1234>
2020-02-10 13:28:47 +0000

    add command to install directly from github

--- rebuilt.bat
+++ rebuilt.bat
@@ -1,3 +1,4 @@
 pip uninstall -y commodplot
 python setup.py bdist_wheel
-pip install dist\commodplot-1.2.0-py2.py3-none-any.whl
+pip install dist\commodplot-1.3.0-py2.py3-none-any.whl
+REM pip install git+https://github.com/aeorxc/commodplot#egg=commodplot
