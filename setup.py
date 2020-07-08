6c15137b9a1191b32b48d7078e36ffae0b38d11e
UI906879 <Pear2222>
2020-07-08 13:12:18 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setuptools.setup(
     name="commodplot",
-    version="1.0.5",
+    version="1.0.6",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity plotting including seasonal charts",
