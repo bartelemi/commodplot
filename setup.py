734f89fa903453e5898045a45e0fddf55716c648
UI906879 <Pear2222>
2020-11-04 12:45:55 +0000

    fix requirements-dev.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodplot",
-    version="1.0.20",
+    version="1.0.21",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity plotting including seasonal charts using plotly",
