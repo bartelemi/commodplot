3423bb1e63a798d3b009c2f73c8fcf8b6ea14833
UI906879 <gibran.afzal@rwe.com>
2020-12-10 18:06:13 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodplot",
-    version="1.1.0",
+    version="1.1.1",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity plotting including seasonal charts using plotly",
