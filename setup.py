cf205ed8e6c0ab63aaf5cfdb3bd2eeaa17192f81
ui906879 <Bear11112222>
2021-11-15 12:06:58 +0000

    fix: failing test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodplot",
-    version="1.3.0",
+    version="1.3.1",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity plotting including seasonal charts using plotly",
