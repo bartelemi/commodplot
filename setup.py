ea0cfca6dec032111ab9899718cc8b44c599952a
ui906879 <Bear11112222>
2022-03-18 14:26:48 +0000

    feat: add precision parameter for tables

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodplot",
-    version="1.3.8",
+    version="1.3.9",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity plotting including seasonal charts using plotly",
