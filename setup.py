e92c5b771bc4aea6d4743cfdfb7fb4a4e6bcbb41
ui906879 <Bear11112222>
2022-03-30 16:46:11 +0100

    chore: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodplot",
-    version="1.3.11",
+    version="1.3.13",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity plotting including seasonal charts using plotly",
